11e8fc7a234425bc621407b541dd779629308d59
Lewis O'Driscoll <lewis.odriscoll@credera.co.uk>
2023-12-04 20:57:56 +0000

    refactor: ready for part 2

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -1,5 +1,32 @@
 use std::{collections::HashSet, fs};
 
+pub struct Scratchcard {
+    winning_numbers: HashSet<u32>,
+    my_numbers: HashSet<u32>,
+}
+
+impl Scratchcard {
+    pub fn parse(row: &str) -> Scratchcard {
+        let parts: Vec<&str> = row.split("|").collect();
+        let winning_numbers = Scratchcard::parse_numbers(parts[0]);
+        let my_numbers = Scratchcard::parse_numbers(parts[1]); 
+        Scratchcard { winning_numbers, my_numbers }
+    }
+
+    fn parse_numbers(numbers: &str) -> HashSet<u32> {
+        numbers
+            .trim()
+            .split(' ')
+            .filter(|number| !number.is_empty())
+            .map(|number| number.parse().unwrap())
+            .collect()
+    }
+
+    pub fn num_matches(&self) -> u32 {
+        self.winning_numbers.intersection(&self.my_numbers).count() as u32
+    }
+}
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let file_name = args.get(1).unwrap();
@@ -8,23 +35,10 @@ fn main() {
         .unwrap()
         .lines()
         .map(|line| line.split(":").last().unwrap())
-        .map(|numbers| {
-            let parts: Vec<&str> = numbers.split("|").collect();
-            let winning_numbers = parse_numbers(parts[0]);
-            let my_numbers = parse_numbers(parts[1]);
-            winning_numbers.intersection(&my_numbers).count()
-        })
+        .map(|numbers| Scratchcard::parse(numbers))
+        .map(|card| card.num_matches())
         .filter(|num_matches| *num_matches != 0)
-        .map(|num_matches| 2u32.pow((num_matches - 1) as u32))
+        .map(|num_matches| 2u32.pow(num_matches - 1))
         .sum();
     println!("Total: {}", total_score)
-}
-
-fn parse_numbers(numbers: &str) -> HashSet<u32> {
-    numbers
-        .trim()
-        .split(' ')
-        .filter(|number| !number.is_empty())
-        .map(|number| number.parse().unwrap())
-        .collect()
 }
