a2c1f64064d8ede8c8e4157e3f8d7fc8cda3df1e
Lewis O'Driscoll <lewis.odriscoll@credera.co.uk>
2023-12-10 16:10:13 +0000

    feat: day 7 part 2 - not working :(

--- day-7/src/main.rs
+++ day-7/src/main.rs
@@ -73,6 +73,26 @@ impl Hand {
             *count += 1
         }
 
+        if counts.contains_key(&Card::Joker) && *counts.get(&Card::Joker).unwrap() != 5 {
+            let (most_common_card, max_count) = counts
+                .iter()
+                .filter(|(card, _)| *card != &Card::Joker)
+                .max_by_key(|(_card, count)| *count)
+                .unwrap_or_else(|| {
+                    let first_non_joker = counts
+                        .keys()
+                        .filter(|card| *card != &Card::Joker)
+                        .next()
+                        .unwrap();
+                    (first_non_joker, &counts[first_non_joker])
+                });
+            counts.insert(*most_common_card, max_count + counts[&Card::Joker]);
+        }
+
+        Hand::get_type_from_counts(counts)
+    }
+
+    fn get_type_from_counts(counts: HashMap<Card, u32>) -> HandType {
         if counts.values().any(|count| *count == 5) {
             HandType::FiveOfaKind
         } else if counts.values().any(|count| *count == 4) {
@@ -98,6 +118,7 @@ impl Hand {
 
 #[derive(Ord, PartialOrd, Eq, PartialEq, Debug, Copy, Clone, Hash)]
 enum Card {
+    Joker,
     Number(u32),
     Picture(Royal),
     Ace,
@@ -106,13 +127,12 @@ enum Card {
 impl Card {
     const KING: Card = Card::Picture(Royal::King);
     const QUEEN: Card = Card::Picture(Royal::Queen);
-    const JACK: Card = Card::Picture(Royal::Jack);
 
     fn parse(character: char) -> Card {
         match character {
             'K' => Card::KING,
             'Q' => Card::QUEEN,
-            'J' => Card::JACK,
+            'J' => Card::Joker,
             'T' => Card::Number(10),
             'A' => Card::Ace,
             _ => Card::Number(character.to_digit(10).unwrap()),
@@ -122,7 +142,6 @@ impl Card {
 
 #[derive(Ord, PartialOrd, Eq, PartialEq, Debug, Copy, Clone, Hash)]
 enum Royal {
-    Jack,
     Queen,
     King,
 }
@@ -144,16 +163,6 @@ mod tests {
     use crate::Card::Number;
 
     #[test]
-    fn should_order_picture_cards() {
-        let mut cards = vec![Card::KING, Card::QUEEN, Card::JACK];
-
-        cards.sort();
-
-        let expected = vec![Card::JACK, Card::QUEEN, Card::KING];
-        assert_eq!(expected, cards);
-    }
-
-    #[test]
     fn should_order_number_cards() {
         let mut cards = vec![Number(3), Number(8), Number(5)];
 
@@ -165,11 +174,11 @@ mod tests {
 
     #[test]
     fn should_order_number_and_picture_cards() {
-        let mut cards = vec![Number(3), Card::KING, Number(8), Card::JACK, Number(5)];
+        let mut cards = vec![Number(3), Card::KING, Number(8), Card::QUEEN, Number(5)];
 
         cards.sort();
 
-        let expected = vec![Number(3), Number(5), Number(8), Card::JACK, Card::KING];
+        let expected = vec![Number(3), Number(5), Number(8), Card::QUEEN, Card::KING];
         assert_eq!(expected, cards);
     }
 
@@ -181,7 +190,7 @@ mod tests {
             bet: 0,
         };
         let two = Hand {
-            cards: vec![Card::KING, Number(10), Card::JACK, Card::JACK, Number(10)],
+            cards: vec![Card::KING, Number(10), Card::QUEEN, Card::QUEEN, Number(10)],
             hand_type: HandType::TwoPair,
             bet: 0,
         };
@@ -195,6 +204,36 @@ mod tests {
         cards.sort();
 
         let expected = vec![one, two, three];
+        assert_eq!(expected, cards);
+    }
+
+    #[test]
+    fn should_order_hands_by_type_then_cards_with_jokers() {
+        let one = Hand {
+            cards: vec![Number(3), Number(2), Number(10), Number(3), Card::KING],
+            hand_type: HandType::OnePair,
+            bet: 0,
+        };
+        let two = Hand {
+            cards: vec![Card::KING, Card::Joker, Number(10), Card::Joker, Number(10)],
+            hand_type: HandType::FourOfAKind,
+            bet: 0,
+        };
+        let three = Hand {
+            cards: vec![Card::KING, Card::Joker, Card::KING, Card::Joker, Number(10)],
+            hand_type: HandType::FourOfAKind,
+            bet: 0,
+        };
+        let four = Hand {
+            cards: vec![Card::KING, Number(2), Number(2), Number(2), Number(2)],
+            hand_type: HandType::FourOfAKind,
+            bet: 0,
+        };
+        let mut cards = vec![three.clone(), one.clone(), four.clone(), two.clone()];
+
+        cards.sort();
+
+        let expected = vec![one, two, three, four];
         assert_eq!(expected, cards);
     }
 
@@ -227,7 +266,7 @@ mod tests {
 
     #[test]
     fn should_calculate_three_of_a_kind() {
-        let cards = vec![Number(3), Card::KING, Number(3), Number(3), Card::JACK];
+        let cards = vec![Number(3), Card::KING, Number(3), Number(3), Card::QUEEN];
 
         let hand_type = Hand::get_type(&cards);
 
@@ -236,7 +275,7 @@ mod tests {
 
     #[test]
     fn should_calculate_two_pair() {
-        let cards = vec![Number(3), Card::KING, Card::JACK, Number(3), Card::JACK];
+        let cards = vec![Number(3), Card::KING, Card::QUEEN, Number(3), Card::QUEEN];
 
         let hand_type = Hand::get_type(&cards);
 
@@ -245,7 +284,7 @@ mod tests {
 
     #[test]
     fn should_calculate_one_pair() {
-        let cards = vec![Number(3), Card::KING, Card::JACK, Number(3), Number(8)];
+        let cards = vec![Number(3), Card::KING, Card::QUEEN, Number(3), Number(8)];
 
         let hand_type = Hand::get_type(&cards);
 
@@ -254,7 +293,7 @@ mod tests {
 
     #[test]
     fn should_calculate_high_card() {
-        let cards = vec![Number(3), Card::KING, Card::JACK, Number(9), Number(8)];
+        let cards = vec![Number(3), Card::KING, Card::QUEEN, Number(9), Number(8)];
 
         let hand_type = Hand::get_type(&cards);
 
@@ -263,13 +302,137 @@ mod tests {
 
     #[test]
     fn should_parse_hand() {
+        let row = "T55Q5 684";
+
+        let hand = Hand::parse(row);
+
+        let expected_hand = Hand {
+            cards: vec![Number(10), Number(5), Number(5), Card::QUEEN, Number(5)],
+            hand_type: HandType::ThreeOfAKind,
+            bet: 684,
+        };
+        assert_eq!(expected_hand, hand);
+    }
+
+    #[test]
+    fn should_parse_hand_with_joker_four_of_a_kind() {
         let row = "T55J5 684";
 
         let hand = Hand::parse(row);
 
         let expected_hand = Hand {
-            cards: vec![Number(10), Number(5), Number(5), Card::JACK, Number(5)],
+            cards: vec![Number(10), Number(5), Number(5), Card::Joker, Number(5)],
+            hand_type: HandType::FourOfAKind,
+            bet: 684,
+        };
+        assert_eq!(expected_hand, hand);
+    }
+
+    #[test]
+    fn should_parse_hand_with_joker_one_pair() {
+        let row = "T58J2 684";
+
+        let hand = Hand::parse(row);
+
+        let expected_hand = Hand {
+            cards: vec![Number(10), Number(5), Number(8), Card::Joker, Number(2)],
+            hand_type: HandType::OnePair,
+            bet: 684,
+        };
+        assert_eq!(expected_hand, hand);
+    }
+
+    #[test]
+    fn should_parse_hand_with_joker_three_of_a_kind() {
+        let row = "TT8J2 684";
+
+        let hand = Hand::parse(row);
+
+        let expected_hand = Hand {
+            cards: vec![Number(10), Number(10), Number(8), Card::Joker, Number(2)],
             hand_type: HandType::ThreeOfAKind,
+            bet: 684,
+        };
+        assert_eq!(expected_hand, hand);
+    }
+
+    #[test]
+    fn should_parse_hand_with_joker_five_of_a_kind() {
+        let row = "TTTJT 684";
+
+        let hand = Hand::parse(row);
+
+        let expected_hand = Hand {
+            cards: vec![Number(10), Number(10), Number(10), Card::Joker, Number(10)],
+            hand_type: HandType::FiveOfaKind,
+            bet: 684,
+        };
+        assert_eq!(expected_hand, hand);
+    }
+
+    #[test]
+    fn should_parse_hand_with_joker_full_house() {
+        let row = "88J55 684";
+
+        let hand = Hand::parse(row);
+
+        let expected_hand = Hand {
+            cards: vec![Number(8), Number(8), Card::Joker, Number(5), Number(5)],
+            hand_type: HandType::FullHouse,
+            bet: 684,
+        };
+        assert_eq!(expected_hand, hand);
+    }
+
+    #[test]
+    fn should_parse_hand_of_all_jokers() {
+        let row = "JJJJJ 684";
+
+        let hand = Hand::parse(row);
+
+        let expected_hand = Hand {
+            cards: vec![
+                Card::Joker,
+                Card::Joker,
+                Card::Joker,
+                Card::Joker,
+                Card::Joker,
+            ],
+            hand_type: HandType::FiveOfaKind,
+            bet: 684,
+        };
+        assert_eq!(expected_hand, hand);
+    }
+
+    #[test]
+    fn should_parse_hand_of_four_jokers() {
+        let row = "JJ5JJ 684";
+
+        let hand = Hand::parse(row);
+
+        let expected_hand = Hand {
+            cards: vec![
+                Card::Joker,
+                Card::Joker,
+                Number(5),
+                Card::Joker,
+                Card::Joker,
+            ],
+            hand_type: HandType::FiveOfaKind,
+            bet: 684,
+        };
+        assert_eq!(expected_hand, hand);
+    }
+
+    #[test]
+    fn should_parse_hand_of_three_jokers() {
+        let row = "JJ5J4 684";
+
+        let hand = Hand::parse(row);
+
+        let expected_hand = Hand {
+            cards: vec![Card::Joker, Card::Joker, Number(5), Card::Joker, Number(4)],
+            hand_type: HandType::FourOfAKind,
             bet: 684,
         };
         assert_eq!(expected_hand, hand);
