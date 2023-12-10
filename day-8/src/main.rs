6766f440bd4ccde84ad6691ed6dbad2674311303
Lewis O'Driscoll <lewis.odriscoll@credera.co.uk>
2023-12-10 19:17:35 +0000

    feat: day 8 part 2

--- day-8/src/main.rs
+++ day-8/src/main.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use std::fs;
+use num::integer::lcm;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -7,7 +8,8 @@ fn main() {
 
     let file = fs::read_to_string(file_name).unwrap();
     let mut lines = file.lines();
-    let directions = lines.next().unwrap().chars().cycle();
+    let directions_line = lines.next().unwrap();
+    let directions = directions_line.chars().cycle();
     lines.next(); // Skip whitespace
     let nodes: HashMap<String, Node> = lines
         .map(|line| Node::parse(line))
@@ -17,19 +19,38 @@ fn main() {
     let mut current = nodes.get(Node::START).unwrap();
     let mut num_steps = 0u64;
     for direction in directions {
-        let next_label = match direction {
-            'L' => &current.left,
-            'R' => &current.right,
-            _ => panic!("Unknown direction: {direction}"),
-        };
-        current = nodes.get(next_label).unwrap();
+        current = step(&nodes, current, direction);
         num_steps += 1;
         if current.label == Node::END {
             break;
         }
     }
-
     println!("Part 1: {num_steps}");
+
+    let mut current_nodes: Vec<&Node> = nodes.values()
+        .filter(|node| node.is_start())
+        .collect();
+    println!("Num ghosts: {}", current_nodes.len());
+    let end_distances: u64 = current_nodes.iter().map(|node| {
+        let mut directions = directions_line.chars().cycle();
+        let mut current = *node;
+        let mut num_steps: u64 = 0;
+        while !current.is_end() {
+            current = step(&nodes, current, directions.next().unwrap());
+            num_steps += 1;
+        }
+        num_steps
+    }).reduce(|x, y| lcm(x, y)).unwrap();
+    println!("End distances: {:?}", end_distances);
+}
+
+fn step<'a>(nodes: &'a HashMap<String, Node>, current_node: &Node, direction: char) -> &'a Node {
+    let next_label = match direction {
+        'L' => &current_node.left,
+        'R' => &current_node.right,
+        _ => panic!("Unknown direction: {direction}"),
+    };
+    return nodes.get(next_label).unwrap();
 }
 
 #[derive(Debug, Eq, PartialEq)]
@@ -37,6 +58,16 @@ struct Node {
     label: String,
     left: String,
     right: String,
+}
+
+impl Node {
+    fn is_start(&self) -> bool {
+        self.label.ends_with('A')
+    }
+
+    fn is_end(&self) -> bool {
+        self.label.ends_with('Z')
+    }
 }
 
 impl Node {
